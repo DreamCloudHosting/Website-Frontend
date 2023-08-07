651a1b1a122568e2ff3e36ab2a9e438a32e3f463
Kyle Wong <kylecom2017@gmail.com>
2023-08-08 00:23:18 +0800

    added header

--- app/page.js
+++ app/page.js
@@ -1,3 +1,15 @@
 export default function Page() {
-    return <body><h1>Welcome to Dream Cloud Hosting</h1><p>The best webhosting service</p></body>;
+    return(
+        <><header>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="news">News</a></li>
+                <li><a href="contactus">Contact Us</a></li>
+                <li><a href="aboutus">About Us</a></li>
+            </ul>
+        </header><body>
+            <h1>Welcome to Dream Cloud Hosting</h1>
+            <p>The best webhosting service</p>
+        </body></>
+    );
 }
