9749da9bd7745d92f588e047c42abb439c9ef6e9
OnCloud <anonymousaaaa41414141@gmail.com>
2023-08-16 14:28:42 +0800

    Improve accessibility

--- components/Navbar/components/Option/Option.js
+++ components/Navbar/components/Option/Option.js
@@ -5,11 +5,13 @@ import styles from "./Option.module.css";
 
 export default function Option({ title, link }) {
     return (
-        <Link href={link || "#"} className={styles.option}>
-            <p>{title}</p>
-            <div className={styles.leftBorder}></div>
-            <div className={styles.topBorder}></div>
-            <div className={styles.rightBorder}></div>
-        </Link>
+        <li className={styles.container}>
+            <Link href={link || "#"} className={styles.option}>
+                <p>{title}</p>
+                <div className={styles.leftBorder}></div>
+                <div className={styles.topBorder}></div>
+                <div className={styles.rightBorder}></div>
+            </Link>
+        </li>
     );
 }
